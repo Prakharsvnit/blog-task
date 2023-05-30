3bc9e5842cba0d26fbb322e2cc9bb0e68e7fcc26
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-30 21:20:36 +0530

    Basic draft

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
+import { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [editorState, setEditorState] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleEditorChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
+  const handleSubmit = () => {
+    // Validate title and description before submitting
+    if (title.trim() === "") {
+      alert("Please enter a title.");
+      return;
+    }
+
+    if (description.trim() === "") {
+      alert("Please enter a description.");
+      return;
+    }
+
+    const contentState = editorState.getCurrentContent();
+    const contentHtml = draftToHtml(convertToRaw(contentState));
+
+    // Submit the blog post with title, description, and contentHtml
+    // ...
+
+    // Clear the form after successful submission
+    setTitle("");
+    setDescription("");
+    setEditorState("");
+  };
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div>
+      <label>Title:</label>
+      <input type="text" value={title} onChange={handleTitleChange} />
+
+      <label>Description:</label>
+      <input
+        type="text"
+        value={description}
+        onChange={handleDescriptionChange}
+      />
+
+      <label>Content:</label>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={handleEditorChange}
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
   );
 }
 
